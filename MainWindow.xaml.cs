d66dc713aed968697ea63cad7181f4829d04bade
Student 241580 <241580@student.pwr.edu.pl>
2020-03-15 19:40:59 -0700

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Calendar
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            GetProperlyDirectory();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            EventsDBContext db = new EventsDBContext();
+            var contents = db.Events;
+            foreach (var item in contents)
+                label.Content = item.Day;
+        }
+
+        private void GetProperlyDirectory()
+        {
+            string Path = Environment.CurrentDirectory;
+            string[] appPath = Path.Split(new string[] { "bin" }, StringSplitOptions.None);
+            AppDomain.CurrentDomain.SetData("DataDirectory", appPath[0]);
+
+        }
+    }
+}
